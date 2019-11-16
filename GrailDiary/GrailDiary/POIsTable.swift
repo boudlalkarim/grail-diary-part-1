a3fd0a2065dd7128195c7c3ab8a1960c73a2731c
Lambda_School_Loaner_228 <lambda_school_loaner_228@Lambda-School-Loaner-228s-MacBook-Pro.local>
2019-11-15 22:19:40 -0500

    checking

---
+++ GrailDiary/GrailDiary/POIsTable.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Lambda_School_Loaner_228 on 11/15/19.
+//  Copyright © 2019 Lambda_School_Loaner_228. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
